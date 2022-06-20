774f0a358bb5a93fed2acd6c2aef271f58b614cf
nagaoka1166 <Ryuu11660408@gmail.com>
2022-06-20 18:00:21 +0900

    headerの追加

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class RestaurantszController <  ApplicationController
+    class RestaurantsController <  ApplicationController
       def index
         restaurants = Restaurant.all
 
