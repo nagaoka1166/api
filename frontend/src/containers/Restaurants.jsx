2b9007796c0c41c6826c09cc8f6d9e2328d0feb5
nagaoka1166 <Ryuu11660408@gmail.com>
2022-06-23 18:03:56 +0900

    reducer

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,8 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 import styled from 'styled-components';
 
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
+
+import {
+  initialState,
+  restaurantsActionTyps,
+  restaurantsReducer,
+} from '../reducers/restaurants';
 
 // images
 import MainLogo from '../images/logo.png';
@@ -26,22 +32,36 @@ const MainCover = styled.img`
   height: 600px;
 `;
 
-export const Restaurants = () => {
-  useEffect(() => {
-    fetchRestaurants()
-      .then((data) =>
-        console.log(data)
-      )
-  }, [])
+const [state, dispatch] = useReducer(restaurantsReducer, initialState);
 
-  return (
-    <Fragment>
-      <HeaderWrapper>
-        <MainLogoImage src={MainLogo} alt="main logo" />
-      </HeaderWrapper>
-      <MainCoverImageWrapper>
-        <MainCover src={MainCoverImage} alt="main cover" />
-      </MainCoverImageWrapper>
-    </Fragment>
-  )
+useEffect(() => {
+  dispatch({ type: restaurantsActionTyps.FETCHING });
+  fetchRestaurants()
+    .then((data) =>
+      dispatch({
+        type: restaurantsActionTyps.FETCH_SUCCESS,
+        payload: {
+          restaurants: data.restaurants
+        }
+      })
+    )
+}, [])
+
+return (
+  <Fragment>
+    <HeaderWrapper>
+      <MainLogoImage src={MainLogo} alt="main logo" />
+    </HeaderWrapper>
+    <MainCoverImageWrapper>
+      <MainCover src={MainCoverImage} alt="main cover" />
+    </MainCoverImageWrapper>
+    {
+      state.restaurantsList.map(restaurant =>
+        <div>
+          {restaurant.name}
+        </div>
+      )
+    }
+  </Fragment>
+)
 }
